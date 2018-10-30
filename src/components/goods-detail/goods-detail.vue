a0ad5ba27feac76d6ff2f5b97fbcb66665610adf
VarHug <490641570@qq.com>
2018-10-30 13:43:55 +0800

    从后端获取商品详情

--- src/components/goods-detail/goods-detail.vue
+++ src/components/goods-detail/goods-detail.vue
@@ -1,21 +1,34 @@
 <template>
-  <div class="goods-detail">
-    <div class="goods-id">{{productId}}</div>
+  <div class="goods-detail" v-if="product">
     <div class="product-intro">
       <div class="preview-wrap">
         <div class="preview">
           <div class="main-img">
-            <img width="450" src="//img10.360buyimg.com/n1/s450x450_jfs/t8287/299/1334040710/28963/780fbd17/59b857feN9690dbe6.jpg">
+            <img width="450" :src="getMainImg">
           </div>
-          <div class="spec-list"></div>
+          <div class="spec-list">
+            <i class="el-icon-arrow-left" @click="scrollLeft"></i>
+            <div class="spec-items">
+              <ul class="lh" ref="lh">
+                <li class="lh-item"
+                  v-for="(item, index) in product.preview"
+                  :key="index"
+                  :class="{'curr': previewIndex === index}"
+                  @mouseover.stop.prevent="changePreview(index)">
+                  <img :src="item.small">
+                </li>
+              </ul>
+            </div>
+            <i class="el-icon-arrow-right" @click="scrollRight"></i>
+          </div>
         </div>
       </div>
       <div class="itemInfo-wrap">
-        <div class="sku-name">Apple iPhone 8 Plus (A1864) 64GB 深空灰色 移动联通电信4G手机</div>
-        <div class="news">后置双摄，双面玻璃，经典之选，美在智慧，XS系列享受12期白条免息</div>
+        <div class="sku-name">{{product.pName}}</div>
+        <div class="news">{{product.pTitle}}</div>
         <div class="price">
           <span class="dt">价格：</span>
-          <span class="p-price">{{5999 | price}}</span>
+          <span class="p-price">{{product.pPrice | price}}</span>
         </div>
         <div class="num">
           <span class="dt">数量：</span>
@@ -26,6 +39,9 @@
           <a href="javascript:;" class="buy-btn buy-cart" @click="addCart">加入购物车</a>
         </div>
       </div>
+    </div>
+    <div class="detail-content">
+      <img v-for="(image, index) in product.detail" :key="index" :src='image'>
     </div>
     <transition
       @before-enter="beforeEnter"
@@ -41,10 +57,13 @@
 
 <script type="text/ecmascript-6">
 import {prefix} from '../../common/js/dom.js'
+import {ERR_OK} from '../../api/config.js'
 
 const transform = prefix('transform')
 const transition = prefix('transition')
-
+const LH_ITEM_WIDTH = 58
+const LH_ITEM_MARGIN = 18
+const LH_ITEM_LEN = 5
 const animationElementWidth = 45
 const animationElementHeight = 45
 
@@ -52,14 +71,35 @@ export default {
   data() {
     return {
       productId: '',
+      product: '',
       isAnimation: false,
-      num: 1
+      num: 1,
+      previewIndex: 0,
+      scrollPos: [],
+      scrollIndex: 0
     }
   },
   computed: {
-
+    getMainImg() {
+      return this.product.preview[this.previewIndex].large
+    }
   },
   methods: {
+    changePreview(index) {
+      this.previewIndex = index
+    },
+    scrollLeft() {
+      if (this.scrollIndex > 0) {
+        this.scrollIndex--
+        this.$refs.lh.style[transform] = `translateX(${this.scrollPos[this.scrollIndex]}px)`
+      }
+    },
+    scrollRight() {
+      if (this.scrollIndex < this.scrollPos.length - 1) {
+        this.scrollIndex++
+        this.$refs.lh.style[transform] = `translateX(${this.scrollPos[this.scrollIndex]}px)`
+      }
+    },
     handleChange(val) {
       console.log(val)
     },
@@ -102,10 +142,38 @@ export default {
     },
     _finishAnimation() {
       this.isAnimation = false
+    },
+    _getGoodDetail() {
+      let param = {
+        productId: this.productId
+      }
+      this.$axios.get('/api/good/detail', {
+        params: param
+      }).then(res => {
+        if (res.data.status === ERR_OK) {
+          this.product = res.data.result.list[0]
+          console.log(this.product)
+          this.$nextTick(() => {
+            this._setLh()
+          })
+        }
+      })
+    },
+    // 设置lh滑动相关参数
+    _setLh() {
+      this.$refs.lh.style.width = (LH_ITEM_WIDTH + LH_ITEM_MARGIN) * this.product.preview.length + 'px'
+      let diff = this.product.preview.length - LH_ITEM_LEN
+      for (let i = 0; i <= diff; i++) {
+        this.scrollPos.push(-i * (LH_ITEM_WIDTH + LH_ITEM_MARGIN))
+      }
     }
   },
   created() {
     this.productId = this.$route.query.productId
+    this._getGoodDetail()
+  },
+  mounted() {
+
   },
   components: {
 
@@ -118,8 +186,11 @@ export default {
 
 .goods-detail
   background #fff
+  margin-bottom 20px
+  padding 10px
   /.product-intro
     display flex
+    margin-bottom 20px
     /.preview-wrap
       margin-right 50px
       width 452px
@@ -128,6 +199,43 @@ export default {
         .main-img
           border 1px solid #eee
           margin-bottom 20px
+          img
+            vertical-align middle
+        .spec-list
+          position relative
+          .el-icon-arrow-left, .el-icon-arrow-right
+            display block
+            height 32px
+            position absolute
+            top 50%
+            margin-top -16px
+            font-size 30px
+            font-weight bold
+            color $color-999
+            cursor pointer
+          .el-icon-arrow-left
+            left 0
+          .el-icon-arrow-right
+            right 0
+          .spec-items
+            position relative
+            margin 0 auto
+            width 380px
+            height 58px
+            overflow hidden
+            .lh
+              height 58px
+              transition all .5s
+              .lh-item
+                float left
+                margin 0 9px
+                img
+                  width 54px
+                  height 54px
+                  border 2px solid #fff
+                &.curr
+                  img
+                    border 2px solid #e53e41
     /.itemInfo-wrap
       flex 1
       .sku-name
@@ -171,6 +279,12 @@ export default {
             width 198px
             border 1px solid #ddd
             color $color-333
+  /.detail-content
+    margin 0 auto
+    width 750px
+    img
+      width 100%
+      vertical-align middle
 
 .animation-wrap
   position fixed

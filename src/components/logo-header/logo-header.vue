0b2981bba4708a0dacbe4d10ee766189bf06025f
VarHug <490641570@qq.com>
2018-11-05 14:55:17 +0800

    分离logo-header组件

---
+++ src/components/logo-header/logo-header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="logo-wrap">
+    <div class="logo">
+      <div class="img-wrapper" @click="toHome">
+        <img width="100%" src="//img10.360buyimg.com/cms/jfs/t17752/324/262367977/74875/fd9a74db/5a65c544N9fcaf6f7.png!q90.jpg">
+      </div>
+      <span class="login-logo-tip">依《网络安全法》相关要求，即日会员登陆需绑定手机。为保障您的账户安全及正常使用，如您尚未绑定，请尽快完成绑定，感谢您的理解和支持!</span>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  methods: {
+    toHome() {
+      this.$router.push('/home')
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~@/common/stylus/mixin.styl'
+
+.logo-wrap
+  padding 20px 0
+  background #fff
+  .logo
+    inner()
+    font-size 0
+    .img-wrapper
+      display inline-block
+      vertical-align middle
+      margin-right 20px
+      width 220px
+      cursor pointer
+    .login-logo-tip
+      display inline-block
+      vertical-align middle
+      padding 0 15px
+      line-height 28px
+      height 28px
+      font-size 11px
+      background #ffefef
+      border 1px solid #ffc0cc
+</style>
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="login">
-    <div class="logo-wrap">
-      <div class="logo">
-        <div class="img-wrapper">
-          <img width="100%" src="//img10.360buyimg.com/cms/jfs/t17752/324/262367977/74875/fd9a74db/5a65c544N9fcaf6f7.png!q90.jpg">
-        </div>
-        <span class="login-logo-tip">依《网络安全法》相关要求，即日会员登陆需绑定手机。为保障您的账户安全及正常使用，如您尚未绑定，请尽快完成绑定，感谢您的理解和支持!</span>
-      </div>
-    </div>
+    <logo-header></logo-header>
     <div class="content">
       <div class="lg-content">
         <div class="lg-form">
@@ -22,6 +15,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+import logoHeader from '../../components/logo-header/logo-header'
 import pFooter from '../../components/p-footer/p-footer'
 import {ERR_OK} from '../../api/config.js'
 import {mapActions, mapGetters, mapMutations} from 'vuex'
@@ -125,36 +119,16 @@ export default {
     }
   },
   components: {
+    logoHeader,
     pFooter
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~@/common/stylus/mixin.styl'
 @import '~@/common/stylus/variable.styl'
 
 .login
-  .logo-wrap
-    padding 20px 0
-    background #fff
-    .logo
-      inner()
-      font-size 0
-      .img-wrapper
-        display inline-block
-        vertical-align middle
-        margin-right 20px
-        width 220px
-      .login-logo-tip
-        display inline-block
-        vertical-align middle
-        padding 0 15px
-        line-height 28px
-        height 28px
-        font-size 11px
-        background #ffefef
-        border 1px solid #ffc0cc
   .content
     position relative
     margin-bottom 20px
